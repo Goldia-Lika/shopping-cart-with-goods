448d0e45b7e21dd0b153b151a14d42e5ee76ed03
Angelina <xgoldiax@gmail.com>
2025-07-28 23:31:02 +0300

    добавила файл селектор, отредактировала sidebar

--- src/js/script.js
+++ src/js/script.js
@@ -71,7 +71,11 @@ async function loadJSON() {
         </div>
         <div class="flex items-center justify-between">
             <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price ? '$' + product.price : ''}</span>
-            <a href="#" class="text-white bg-lime-700 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+            <a href="#" class="text-white bg-lime-700
+             hover:bg-lime-600 focus:ring-4 focus:outline-none
+              focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+               dark:bg-blue-600 dark:hover:bg-blue-700
+                dark:focus:ring-blue-800">Add to cart</a>
         </div>
     </div>
 </div>
---
+++ src/js/selectors.js
@@ -0,0 +1,16 @@
+export const SELECTORS = {
+  /* Обертка счетчика */
+  counterInput: document.querySelector('.counter__input'),
+  /* Текст счетчика */
+  counterText: document.querySelector('.counter__text'),
+  /* Кнопка увеличения счетчика */
+  counterBtnUp: document.querySelector('.counter__btn--up'),
+  /* Кнопка уменьшения счетчика */
+  counterBtnDown: document.querySelector('.counter__btn--down'),
+  /* Кнопка добавления товара в корзину */
+  addToCartBtn: document.querySelector('.btn-primary'),
+  /** Форма добавления товара */
+  addProductForm: document.querySelector('#add-form'),
+  /** Контейнер для отрисовки карточек */
+  productsList: document.querySelector('.products-list'),
+}
