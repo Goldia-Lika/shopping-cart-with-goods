fe1ebce88842c93a1e6f8338c0b4f64c747af852
Angelina <xgoldiax@gmail.com>
2025-08-16 15:38:37 +0300

    task-2.написала функцию создание продуктов и функцию добавление товара

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 import {Sidebar} from './components/sidebar.js'
+import { SELECTORS } from './selectors.js'
 new Sidebar('#sidebar', '#sidebar-open')
 
-const productList = document.querySelector('.products-list') // контейнер для отрисовки товаров
+
 
 
 init()
@@ -13,6 +14,8 @@ function init() {
     loadJSON()
   })
 }
+
+//функция загрузки товаров (WIP)
 
 async function loadJSON() {
   try {
@@ -42,11 +45,11 @@ async function loadJSON() {
         html += `
 <div class="w-full max-w-sm mb-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
   <a href="#">
-    <img class="p-8 rounded-t-lg" src="/images/${imgFile}" alt="product image" />
+  <img class="p-8 rounded-t-lg" src="${product?.imgSrc ? product.imgSrc : '/images/' + imgFile}" alt="product image" />
   </a>
   <div class="px-5 pb-5">
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product?.title || 'Без названия'}</h5>
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product?.name || 'Без названия'}</h5>
         </a>
         <div class="text-sm text-gray-500 dark:text-gray-400 mb-2">${product?.category || ''}</div>
         <div class="flex items-center mt-2.5 mb-5">
@@ -67,7 +70,7 @@ async function loadJSON() {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
             </div>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">${product?.rating || 0}</span>
         </div>
         <div class="flex items-center justify-between">
             <span class="text-3xl font-bold text-gray-900 dark:text-white">${product.price ? '$' + product.price : ''}</span>
@@ -83,8 +86,57 @@ async function loadJSON() {
         `
       })
     }
-    productList.innerHTML = html
+    SELECTORS?.productsList?.insertAdjacentHTML('beforeend', html)
   } catch (error) {
     console.error('Ошибка загрузки данных:', error)
   }
 }
+
+console.log('form', SELECTORS.addProductForm)
+
+/**
+ * функция создания продуктов
+ * @param {object} product - Продукт 
+ */
+async function createProduct (product){
+  try {
+    const response = await fetch('http://localhost:3000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    })
+    if (!response.ok) throw new Error('Ошибка при добавлении продукта')
+      return await response.json()
+  } catch (error) {
+    console.error('Ошибка POST:', error)
+    throw error     
+    }
+}
+
+//Функция добавления товаров через форму (WIP)
+export const addProductToPage = ()=> {
+  if (SELECTORS?.addProductForm) {
+    SELECTORS?.addProductForm?.addEventListener('submit', async (event) => {
+      event.preventDefault() 
+      
+      //достаем форму    
+      const form = event.target
+
+      const values = {
+        name: form.name.value,
+        rating: Number.parseInt(form.rating.value, 10),
+        price: Number.parseInt(form.price.value, 10),
+        category: form.category.value,
+        imgSrc: form.imgSrc.value,
+        description: form.description.value,
+      }
+
+      const result = await createProduct(values)
+      console.log(result)
+    })
+  }
+}  
+
+addProductToPage()
