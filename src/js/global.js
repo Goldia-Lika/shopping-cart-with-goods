4b595adad23112281b36517126a9eff725a84970
Angelina <xgoldiax@gmail.com>
2025-08-27 23:53:42 +0300

    добавит функции на генерацию шаблона  для главной страницы/корзины товаров.Добавит глобальную переменную для товаров в корзине

---
+++ src/js/global.js
@@ -0,0 +1,20 @@
+export const SELECTORS = {
+  /* Обертка счетчика */
+  counterInput: document.querySelector('.counter__input'),
+  /* Текст счетчика */
+  counterText: document.querySelector('.counter__text'),
+  /* Кнопка увеличения счетчика */
+  counterBtnUp: document.querySelector('.counter__btn--up'),
+  /* Кнопка уменьшения счетчика */
+  counterBtnDown: document.querySelector('.counter__btn--down'),
+  /* Кнопки добавления товара в корзину */
+  addToCartBtns: document.querySelectorAll('.basket-btn'),
+  /** Форма добавления товара */
+  addProductForm: document.querySelector('#add-form'),
+  /** Контейнер для отрисовки карточек */
+  productsList: document.querySelector('.products-list'),
+  /**Контейнер для отрисовки карточек в корзине */
+  basketList: document.querySelector('#basket-list'),
+}
+
+export const PRODUCTS_IN_BASKET = []  // массив для хранения товаров в корзине
